b1f4ae5929fe1f50600ad36e5592828882a60670
Andrew Garner <agarner84@gmail.com>
2017-10-15 18:24:10 +1100

    update to react-router-dom as react-router was v3 / pre major rewrite

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
 // import package deps
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import react router deps
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-    // import { Provider } from 'react-redux';
-    // import store, { history } from './store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// new react router
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+// import store config
+import store from './redux/store'
 // import css
-import './css/index.css';
+import './css/index.css'
 // import components
-import App from './components/App';
+import App from './components/App'
 // import root reducer
-    // import rootReducer from './reducers/index.js';
+// import rootReducer from './redux/reducers/index.js'
+
+// set component route paths in overarching RootApp
+const RootApp = () => (
+  <Switch>
+    <Route path='/' component={App}>
+    
+    </Route>
+  </Switch>
+)
 
 ReactDOM.render(
-  <App 
-  />, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <RootApp />
+    </BrowserRouter> 
+  </Provider>
+  , document.getElementById('root'))
